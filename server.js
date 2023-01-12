a0188631323b213b7ada7f9d0518f9125faad775
Vishv Salvi <82429084+Vishvsalvi@users.noreply.github.com>
2023-01-12 22:17:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors")
 app.use(cors())
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json())
 
 const connectDB = require("./db/connect")
